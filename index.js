d0e8cf21088a0961c30820ccf7ba12be81db0cf9
Amber M <ambered@Ambers-MacBook-Air.local>
2026-03-18 18:12:19 -0500

    final fix

--- index.js
+++ index.js
@@ -652,7 +652,8 @@ app.post('/submit', (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
