cdc1a29e285aa616b5171a42c93b1dcde709ffd0
Amber M <ambered@Ambers-MacBook-Air.local>
2026-03-18 17:29:49 -0500

    fix deploy error

--- index.js
+++ index.js
@@ -655,5 +655,4 @@ app.post('/submit', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(\`Server running on http://localhost:\${PORT}\`);
-});
+console.log("Server running on port " + PORT);
